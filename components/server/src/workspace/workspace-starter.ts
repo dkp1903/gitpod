ee808018184687b84ef53d7ea4999f394b8a880d
Pudong <tianshi8650@gmail.com>
2022-01-28 02:43:19 +0800

    Update components/server/src/workspace/workspace-starter.ts
    Co-authored-by: Andrea Falzetti <andreafalzetti@users.noreply.github.com>

--- components/server/src/workspace/workspace-starter.ts
+++ components/server/src/workspace/workspace-starter.ts
@@ -348,7 +348,7 @@ export class WorkspaceStarter {
                 if (!!desktopIdeChoice) {
                     const mappedImage = ideConfig.ideOptions.options[desktopIdeChoice];
                     if (!!mappedImage && mappedImage.image) {
-                        const useLatest = !!user.additionalData?.ideSettings?.useLatestVersion
+                        const useLatest = !!user.additionalData?.ideSettings?.useLatestVersion;
                         configuration.desktopIdeImage = useLatest ? (mappedImage.latestImage ?? mappedImage.image) : mappedImage.image
                     } else if (this.authService.hasPermission(user, "ide-settings")) {
                         // if the IDE choice isn't one of the preconfiured choices, we assume its the image name.
